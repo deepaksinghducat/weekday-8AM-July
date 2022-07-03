e438ec1cfd49c81c231b5d9bde80b8d8b3f6bad8
deepaksinghducat <deepaksinghducat@gmail.com>
2022-07-03 14:41:47 +0530

    upto loops

---
+++ js/basic.js
@@ -0,0 +1,174 @@
+// Variables
+
+// 101010 variable  10
+
+// var name = "fsafdsadfasd";
+
+// console.log(name);
+
+// types of varibles 
+// 1 var
+// 2 let
+// 3 const
+
+// Var
+// var name = "fdsafasdf";
+
+// name = "fdsafasdffdasf";
+
+// console.log(name);
+
+
+// let
+// let name = "fdsafas";
+// name = "fasffdsa"
+// let name = "fasfsda";
+
+// console.log(name);
+
+// const
+// const name ="fsadfasfd";
+// console.log(name);
+
+
+
+// Datatypes
+
+// var name = "sdafasdfsdaf";
+
+// memory -> 12byte
+
+
+// types of datatypes
+// 1. Number
+// 2. String
+// 3. BigInt
+// 4. Boolean
+// 5. Array
+// 6. Null
+// 7. Undefined
+// 8. Object
+// 9. Symbol
+// 10. Typeof
+
+
+// Number
+// var num = Number(10)
+// var num1 = 212;
+
+// console.log(typeof num, typeof num1);
+
+// Array 
+// var array = [10,20,30,40,50,60,70,80,90];
+
+
+// 110 111 112 .. 
+
+// 110 + 1 * 0
+
+// baseAddress + no of byte required *  index(in no);
+
+
+// console.log(array[1]);
+
+// 10 11 12 13
+
+
+
+// String
+
+// var name = "Fdsafsadfsadf";
+
+// var num = 10;
+// console.log(num[0]);
+// console.log(name);
+
+
+// Boolean true and false
+// let bol1 = true;
+// let bol2 = false;
+
+// console.log(typeof bol2);
+
+
+// Undefined
+
+// let a;
+
+// a= 10;
+
+// Null
+
+// var a = null;
+
+// console.log(undefined === null);
+
+// Object
+// var array = ["fsadfasf","fsafsadf"];
+
+// console.log(array[0]);
+
+
+// var obj = {
+//     name: "fdsafasdf"
+// };
+
+// console.log(obj.name);
+
+
+// var array = [{name: "fsafsadf"},{name: "fdasfadf"}];
+// console.log(array);
+
+
+
+// Operators
+
+// var a = 10 + 20;
+
+// console.log(a);
+
+// type of operators unary, binary , ternary, operand
+
+// var a = 10;
+
+
+// unary 
+// a++
+// ++a
+// a--
+// --a
+
+// console.log(++a);
+
+// binary
+// var b = 10;
+// var c = 20;
+
+// var a = b + c;
+
+// console.log(a);
+
+// Ternary ?:
+
+// var a = 11;
+// console.log(a == 10 ? true : false);
+
+
+// a = 10;
+// b = 20;
+
+// a += b;
+
+// console.log(a);
+
+
+// String operator
+
+// let a = 10 + 20;
+
+// let a = "fasdfdasf" + "fsafsaf" + 10
+
+let a = "gdsgdsfg"
+
+// console.log(typeof a);
+// console.log( +a);
---
+++ js/control-structure.js
@@ -0,0 +1,79 @@
+//control stucture
+// if
+// else
+// else if
+// switch
+
+// for
+// forin  
+// forof  
+// foreach 
+// while
+// do-while
+
+// if 
+
+// let a = 11;
+// if (a == 10 ) {
+//     console.log("in if block");
+// }else if ( typeof a  == "number") {
+//     console.log("a is number");
+// } else {
+//     console.log("in else block")
+// }
+
+
+// alert , promt , confirm
+// Switch
+// var key = prompt('enter any number');
+
+// var key = 10;
+
+// switch (key) {
+//     case 10:
+//         console.log("fsafsafd");
+        
+//         break;
+//     default:
+
+//         console.log("defualt");
+//         break;
+// }
+
+
+// loops
+var array =  [10,20,30,40,50,60,70,80,90];
+
+// console.log(array.length);
+
+// for (let index = array.length - 1 ; index >= 0 ; index--) {
+
+//     console.log(array[index]);    
+// }
+
+// for (const key in array) {
+//    console.log(array[key]);
+// }
+
+// for (const iterator of array) {
+//     console.log(iterator);
+// }
+
+// array.forEach(function (el,index) {
+//     console.log(el,index);
+// })
+
+// let i = 10;
+// while (i >= 0) {
+//     console.log(i);
+//     --i;
+// }
+
+// let i = 0;
+
+// do {
+//   console.log(i);
+//   i--;  
+// } while (i > 0);
+
+
