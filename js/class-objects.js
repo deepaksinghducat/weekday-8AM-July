2c82533381e0b7910284bb5151293dc59fb6e9be
deepaksinghducat <deepaksinghducat@gmail.com>
2022-07-05 09:16:20 +0530

    upto inheritance

---
+++ js/class-objects.js
@@ -0,0 +1,132 @@
+// // Object
+// const products = [
+//     {
+//         title: "fasadsf",
+//         price: 10
+//     },
+//     {
+//         title: "fasf",
+//         price: 10
+//     },
+//     {
+//         title: "fasfasffasd",
+//         price: 10
+//     },
+// ];
+
+// console.log(products);
+
+// class ProductList {
+//     // title = "NULL"
+//     // price = 0
+
+//     constructor(title,price) {
+//         this.title = title
+//         this.price = price
+//     }
+// }
+
+// const productList = {
+//     products : [
+//         {
+//             title: "fasadsf",
+//             price: 10
+//         },
+//         {
+//             title: "fasf",
+//             price: 10
+//         },
+//         {
+//             title: "fasfasffasd",
+//             price: 10
+//         },
+//         {
+//             title: "fasfasffasd",
+//             price: 10
+//         },
+//         // new ProductList("fasdfasf",10)
+//         new ProductList()
+//     ],
+//     render() {
+//         console.log(this.products);
+//     },
+//     addProduct(product) {
+//         this.products.push(product)
+//     }
+// }
+
+// productList.addProduct({
+//     title: "fasfsadf",
+//     price: 20
+// })
+// productList.render();
+
+
+
+// class ProductList {
+//     products = [
+//         {
+//             title: "fasadsf",
+//             price: 10
+//         },
+//         {
+//             title: "fasf",
+//             price: 10
+//         },
+//         {
+//             title: "fasfasffasd",
+//             price: 10
+//         },
+//         {
+//             title: "fasfasffasd",
+//             price: 10
+//         },
+//     ]
+
+//     render() {
+//         console.log(this.products);
+//     }
+// }
+
+// const productList = new ProductList();
+
+// productList.render();
+
+
+
+// Private Protected Public
+// Inheritance 
+
+
+// class Parent {
+//     // bike= "fasf"
+
+//     constructor(bikeName) {
+//         this.bike = bikeName;
+//     }
+
+//     run() {
+//         console.log(this.bike + " run");
+//     }
+// }
+
+// class Child extends Parent {
+
+//     constructor(bikeName) {
+//        super(bikeName)
+//     }
+//     run(){
+//         super.run()
+//     }
+// }
+
+// const child = new Child('TVS');
+
+// child.run();
+
+
+// 1. write a class which take two parameter Firstname , Secondname and one method return Full Name of any person
+
+
+
+
