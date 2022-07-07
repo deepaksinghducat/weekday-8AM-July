d321cb4143bb2e334d076d77b30277ccbdb7fab4
deepaksinghducat <deepaksinghducat@gmail.com>
2022-07-07 09:21:06 +0530

    dom

---
+++ js/dom.js
@@ -0,0 +1,39 @@
+// window.alert('fasfdsa');
+
+// console.log(window.document.getElementById('dom'));
+
+// ID
+// const body = document.getElementById('body');
+// console.log(body);
+
+
+// Classes 
+// const body = document.getElementsByClassName('body1')
+// console.log(body);
+
+// By tag name
+
+// const tag = document.getElementsByTagName('body');
+
+// const tags = document.getElementsByTagNameNS('body');
+// console.log(tags);
+
+// const div2 = document.getElementById("div2")
+//   const div2Paras = div2.getElementsByTagNameNS("http://www.w3.org/1999/xhtml", "p");
+
+//   const num = div2Paras.length;
+
+//   alert("There are " + num + " &lt;p&gt; elements in div2 element")
+
+
+// Query Selector
+// const body = document.querySelectorAll('div');
+// console.log(body);
+
+// Element create 
+
+
+
+// create one tag add some data in it and append to some tag 
+
+
