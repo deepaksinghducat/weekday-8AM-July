47bdf65326aa888a6256d14cc45c3bf4801490fc
deepaksinghducat <deepaksinghducat@gmail.com>
2022-07-12 09:26:35 +0530

    arrays

---
+++ js/array.js
@@ -0,0 +1,159 @@
+// array 
+// const array = [12,"deeepak",{key:"value"}];
+
+
+// defining array
+
+// []
+// new Array
+// Array.of
+// Array.from
+
+// const array = [10,20,30,40,50,60];
+
+// const array = new Array(10,20,30)
+
+
+// const array = Array.of(10,20,30)
+
+// const array = Array.from([10,30,50,90])
+// console.log(array);
+
+
+// push
+
+// const array = [10,20,30,40,50,60];
+// console.log(array);
+
+// array.push(77000);
+
+// console.log(array);
+
+
+// pop
+
+// const array = [10,20,30,40,50,60];
+// console.log(array);
+
+// array.pop();
+
+// console.log(array);
+
+
+// shift
+
+// const array = [10,20,30,40,50,60];
+// console.log(array);
+
+// array.shift();
+
+// console.log(array);
+
+// unshift
+// const array = [10,20,30,40,50,60];
+// console.log(array);
+
+// array.unshift(1000);
+
+// console.log(array);
+
+
+// Splice 
+
+// const array = [10,20,30,40,50,60];
+
+// 10 20 30 40 50 60
+// 0  1  2  3  4  5
+// -6   -5   -4  -3    -2    -1
+
+// console.log(array);
+
+// array.splice(2,4);
+
+// console.log(array);
+
+// slice
+
+// const array = [10,20,30,40,50,60];
+// console.log(array);
+
+// const array1 = array.slice(2,2);
+
+// console.log(array1);
+
+// Concat
+
+// const arr1 = [10,20,30,40,50,60];
+// const arr2 = [40,70,90,100];
+
+// const arr3 = arr2.concat(arr1);
+
+// console.log(arr3);
+
+// indexOf
+
+// const array = [10,20,30,50];
+
+// console.log(array);
+
+// console.log(array.indexOf(100));
+
+// Find and findIndex
+
+// const array = [10,20,30,40,50];
+
+// let num = array.find((num) => {
+//     if(num === 30) return num;
+// });
+
+// let numIndex = array.findIndex((num,index,nums) => {
+//     if(num === 30) return
+// })
+
+// console.log(numIndex);
+
+
+//foreach 
+
+// const array = [10,20,30,40,50,60];
+
+// console.log(array);
+
+// const array2 = [];
+
+// array.forEach((element) => {
+//     array2.push(element*element);
+// })
+
+// console.log(array2);
+
+
+// Map
+
+// const array = [10,20,30,40,50,60];
+
+// const array2 = array.map((element,index,arrays) => {
+//    return element *  element;
+// })
+
+
+// console.log(array2);
+
+
+// Filter 
+
+// const array = [10,20,30,40,50,60,10];
+
+// const array3 = [];
+
+// const array2 = array.filter((value,index,array) => {
+//     if(value == 10) 
+//        return value;
+// })
+
+
+// console.log(array3);
+
+
+
+
